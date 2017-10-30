b2b4ffdf7bd9eb080e1e90591754368a774c6b1d
Pleshcheev Alexander <al39pl@gmail.com>
2017-10-30 11:54:05 +0300

    implemented css bundle

--- utils/stream.js
+++ utils/stream.js
@@ -1,6 +1,7 @@
 'use strict'
 const yargs = require('yargs');
 const through2= require('through2');
+const https = require('https');
 const fs = require('fs');
 const Parsers = require("./parsers.js");
 const pathService = require('path');
@@ -31,17 +32,15 @@ let csvToJson = (path) => {
     stream.pipe(process.stdout)
 }
 
-let csvToJsonWrite = (path) => {  
+let csvToJsonWrite = (path) => {
     let string = "";
-    let stream = fs.createReadStream(path).pipe(through2.obj(function(chunk, enc, callback){        
+    fs.createReadStream(path).pipe(through2.obj(function(chunk, enc, callback){
         string += chunk.toString();
         callback();
-    })).on('data', function () {                  
+    })).on('data', function () {
         fs.createWriteStream(`../data/${pathService.basename(path, pathService.extname(path))}.json`)
-        .write(JSON.stringify( Parsers(string) ))
+            .write(JSON.stringify( Parsers(string) ))
     })
-
-   
 }
 
 let gen = () => {  
@@ -58,6 +57,55 @@ let gen = () => {
     .write(data) 
 }
 
+let findCss = (path, file) => {
+    let data;
+    var cssFiles = fs.readdirSync(path);
+
+    if(!cssFiles.length){
+        return
+    }
+
+    for (let files = 0; files < cssFiles.length; files++){
+        let filepath = path + "/" + cssFiles[files];
+
+        if (fs.lstatSync(filepath).isDirectory()){
+            bundleCss(filepath)
+        }
+        else{
+            let stream = fs.createReadStream(filepath).pipe(through2.obj(function(chunk, enc, callback){
+
+                fs.appendFile("../assets/css/bundle.css", chunk, function (err) {
+                    if (err) console.log('Not saved!');
+                    console.log('CSS saved!');
+                });
+                this.push(chunk)
+                callback();
+            }))
+        }
+    }
+}
+let bundleCss = (path) => {
+    fs.writeFile('../assets/css/bundle.css', "");
+    findCss(path);
+
+    https.get('https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css', (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            fs.appendFile("../assets/css/bundle.css", data, function (err) {
+                if (err) console.log('Not saved!');
+                console.log('CSS saved!');
+            });
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
 const argv = yargs.command("run", "runs the app", {
         action: {
             describe: "Action to perform",
@@ -68,6 +116,11 @@ const argv = yargs.command("run", "runs the app", {
             describe: "File to be modified",
             demand: true,
             alias: 'f'
+        },
+        path: {
+            describe: "Files to be concatenated",
+            demand: false,
+            alias: 'p'
         }
     })
     .help()
@@ -89,6 +142,10 @@ switch(argv.action){
 
     case ("gen"):
         gen(argv.file)
+    break
+
+    case ("bundle-css"):
+        bundleCss(argv.path)
     break
 
     case ("csvToJsonWrite"):
